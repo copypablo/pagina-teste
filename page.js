a7bbaa77285a8bc0b50ce6364418360135c88d76
Pablo Henrique <pablorodrigues426@gmail.com>
2025-05-26 18:36:11 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+function mostrarOferta() {
+    alert("🚀 Oferta desbloqueada! Você acaba de garantir 50% de desconto por tempo limitado!");
+  }
+  
+  function comprarAgora() {
+    window.location.href = "https://seulink.com/checkout"; // substitua pelo seu link real
+  }
+  
